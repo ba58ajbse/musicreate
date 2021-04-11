0fecce4c55d1bf1d7c17180148ddc13b1b864b1f
Administrator <admin@example.com>
2021-04-11 22:18:49 +0900

    setting material ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import React, { ComponentType } from 'react'
+import React, { ComponentType, useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { AppInitialProps } from 'next/app'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseLine from '@material-ui/core/CssBaseline'
+import Head from 'next/head'
 
 import { store } from '../store/store'
+import theme from '../styles/theme'
 
 const MyApp = ({
   Component,
@@ -11,10 +15,26 @@ const MyApp = ({
   Component: ComponentType<AppInitialProps>
   pageProps: AppInitialProps
 }): JSX.Element => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles)
+    }
+  }, [])
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <CssBaseLine />
+          <Component {...pageProps} />
+        </Provider>
+      </ThemeProvider>
+    </>
   )
 }
 
